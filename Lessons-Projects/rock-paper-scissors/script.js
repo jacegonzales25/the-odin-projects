debc8a6721fb833d711c951b8b4a1c5f3323996d
jacegonzales25 <jacegonzales25@gmail.com>
2024-01-04 23:47:58 +0800

    fix: fix prompt logical with && instead of ||

--- Lessons-Projects/rock-paper-scissors/script.js
+++ Lessons-Projects/rock-paper-scissors/script.js
@@ -1,43 +1,60 @@
-
-
-
 function playRound(playerSelection, computerSelection) {
-    
-    
-    try {
-        let playerSelection = prompt("Enter a choice: rock, paper, scissors").toLowerCase();
-        if (playerSelection != 'rock' || playerSelection != 'paper' || playerSelection != 'scissors') {
-            playerSelection = prompt("Please choose one of the following: rock, paper, or scissors");
-            let done = false;
-            
-            while (!done) {
-                playerSelection = prompt("Please choose one of the following: rock, paper, or scissors").toLowerCase();
-                if (playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors') {
-                    done = true;
-                }
-            }
-            console.log(playerSelection);
-            alert(playerSelection);
-        }
-        console.log(playRound(playerSelection, computerSelection));
-    } catch (error) {
-        console.log(error)
-    }
+
+    console.log(playerSelection, computerSelection)
+
+  
 }
 
-
-
-
 function getComputerChoice() {
-    const randomizer = Math.floor(Math.random() * (choices.length));
-    // console.log(choices)
-    // console.log(choices[randomizer]);
-    return choices[randomizer];
-}   
+  const randomizer = Math.floor(Math.random() * choices.length);
+  // console.log(choices)
+  // console.log(choices[randomizer]);
+  return choices[randomizer];
+}
 
-const choices = ['rock', 'paper', 'scissors']
+function getPlayerChoice() {
+    let playerChoice = prompt(
+      "Enter a choice: rock, paper, scissors"
+    ).toLowerCase();
+    console.log(playerChoice);
+    if (
+      playerChoice != "rock" &&
+      playerChoice != "paper" &&
+      playerChoice != "scissors"
+    ) {
+      playerChoice = prompt(
+        "Please choose one of the following: rock, paper, or scissors"
+      );
+      if (
+        playerChoice != "rock" &&
+        playerChoice != "paper" &&
+        playerChoice != "scissors"
+      ) {
+        let done = false;
+        while (!done) {
+          playerChoice = prompt(
+            "Please choose one of the following: rock, paper, or scissors"
+          ).toLowerCase();
+          if (
+            playerChoice === "rock" ||
+            playerChoice === "paper" ||
+            playerChoice === "scissors"
+          ) {
+            done = true;
+          }
+        }
+      }
+    //   console.log(playerChoice);
+    //   alert(playerChoice);
+    }
+    return playerChoice;
+  
+}
+
+const choices = ["rock", "paper", "scissors"];
 
 const computerSelection = getComputerChoice();
 
+const playerSelection = getPlayerChoice();
 
-getComputerChoice()
+playRound(playerSelection, computerSelection)
