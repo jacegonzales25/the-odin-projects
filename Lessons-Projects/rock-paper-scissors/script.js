f7aae9a3edcc43f52ccae3111c6d41862efd1c91
jacegonzales25 <jacegonzales25@gmail.com>
2024-01-04 23:37:33 +0800

    feat: add player selection

--- Lessons-Projects/rock-paper-scissors/script.js
+++ Lessons-Projects/rock-paper-scissors/script.js
@@ -1,11 +1,43 @@
+
+
+
+function playRound(playerSelection, computerSelection) {
+    
+    
+    try {
+        let playerSelection = prompt("Enter a choice: rock, paper, scissors").toLowerCase();
+        if (playerSelection != 'rock' || playerSelection != 'paper' || playerSelection != 'scissors') {
+            playerSelection = prompt("Please choose one of the following: rock, paper, or scissors");
+            let done = false;
+            
+            while (!done) {
+                playerSelection = prompt("Please choose one of the following: rock, paper, or scissors").toLowerCase();
+                if (playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors') {
+                    done = true;
+                }
+            }
+            console.log(playerSelection);
+            alert(playerSelection);
+        }
+        console.log(playRound(playerSelection, computerSelection));
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+
+
 function getComputerChoice() {
-    const choices = ['rock', 'paper', 'scissors'];
     const randomizer = Math.floor(Math.random() * (choices.length));
-    console.log(choices.length);
-    console.log(choices);
-    console.log(randomizer);
-    console.log(choices[randomizer]);
+    // console.log(choices)
+    // console.log(choices[randomizer]);
     return choices[randomizer];
 }   
+
+const choices = ['rock', 'paper', 'scissors']
+
+const computerSelection = getComputerChoice();
+
 
 getComputerChoice()
